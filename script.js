0b8bec02c7de93dca17dd496c484fae703e27a95
JoaoMateus98 <joaobsu98@gmail.com>
2022-05-26 13:43:16 -0400

    js- when numbers are clicked the appear on screen, all operators, AC button, and equals button work properly

--- script.js
+++ script.js
@@ -1,33 +1,80 @@
-function add (num1, num2) {
-    return num1 + num2;
+const screenOutput = document.querySelector('.screen-output');
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operators');
+const resetButton = document.querySelector('#ac');
+
+console.log(resetButton);
+
+let curNumber = '';
+let storedVal = '';
+let operator = '';
+
+function add (storedVal, curNumber) {
+    return storedVal + curNumber;
 }
 
-function subtract (num1, num2) {
-    return num1 - num2;
+function subtract (storedVal, curNumber) {
+    return storedVal - curNumber;
 }
 
-function multiply (num1, num2) {
-    return num1 * num2;
+function multiply (storedVal, curNumber) {
+    return storedVal * curNumber;
 }
 
-function divide (num1, num2) {
-    return num1 / num2;
+function divide (storedVal, curNumber) {
+    return storedVal / curNumber;
 }
 
-function operate (operator, num1, num2) {
+function operate (operator, storedVal, curNumber) {
     switch (operator) {
         case 'add':
-            return add(num1, num2);
+            return add(storedVal, curNumber)
         case 'subtract':
-            return subtract(num1, num2);
+            return subtract(storedVal, curNumber);
         case 'multiply':
-             return multiply(num1, num2);
+             return multiply(storedVal, curNumber);
         case 'divide':
-            return divide(num1, num2);
+            return divide(storedVal, curNumber);
         default:
-            console.log('Invalid operator');
-            break;
+            return;
     }
 }
 
-console.log(operate('multiply', 5, 5));
+numberButtons.forEach((number) => number.addEventListener('click', (e) => {
+    if (operator != 'equals') {
+    curNumber += e.target.id;
+    } else {
+        return;
+    }
+    screenOutput.textContent = curNumber;
+}))
+
+operatorButtons.forEach((operatorInput) => operatorInput.addEventListener('click', checkOperation));
+
+// 
+function checkOperation (e)  {
+    if (operator.length === 0 && curNumber.length > 0){
+        operator = e.target.id;
+        storedVal = Number.parseInt(curNumber);  
+        curNumber = '';
+
+        screenOutput.textContent = storedVal;
+        return;
+    }
+
+    else if (operator.length > 0 && curNumber.length > 0) {
+        storedVal = operate(operator, storedVal, Number.parseInt(curNumber));
+        curNumber = '';
+
+        screenOutput.textContent = storedVal;
+    }
+    operator = e.target.id;
+};
+
+// resets all the values
+resetButton.addEventListener('click', () => {
+    curNumber = '';
+    storedVal = '';
+    operator = '';
+    screenOutput.textContent = '';
+})
